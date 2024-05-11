255c0a08c0537674097095198df21f38c63fb067
Diav3l <116712498+Diav3l@users.noreply.github.com>
2024-05-10 23:13:36 -0600

    Update Startup.js

--- Startup.js
+++ Startup.js
@@ -1,11 +1,14 @@
 /** @param {NS} ns */
-export async function main(ns) {
-	ns.run('Core/ServerFarmUpgrade.js',1,1);
-	if(ns.getPlayer().mults.hacking_exp<5)
-		ns.run('Core/HacknetMGM.js',1,6);
+export async function main(ns){
+	/**Hacknet setup */
+	let hacknetMults = ns.getHacknetMultipliers()
+	let hacknetservers = Math.floor(Math.log(hacknetMults.production/hacknetMults.purchaseCost)+1)*4
+	if(hacknetservers>0)
+		ns.run('Core/HacknetMGM.js',1,hacknetservers);
+	/**All else */
+	ns.run('Core/ServerFarmUpgrade.js',1,15);
 	ns.run('Core/Worm.js');
-	ns.run('Dispatcher.js');
+	ns.run('Core/Dispatcher.js');
 	ns.run('Core/GangMGM.js');
-	
 	//ns.run('Core/CreatePrograms.js');
 }
